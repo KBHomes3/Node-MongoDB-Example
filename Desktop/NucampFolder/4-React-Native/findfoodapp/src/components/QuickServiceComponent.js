612ca14c4f60b9fb58d68a5ad30598a87ba60214
Alex Aucestovar <aucestovara@gmail.com>
2020-03-03 15:25:29 -0600

    Combining Reducers

--- Desktop/NucampFolder/4-React-Native/findfoodapp/src/components/QuickServiceComponent.js
+++ Desktop/NucampFolder/4-React-Native/findfoodapp/src/components/QuickServiceComponent.js
@@ -3,13 +3,13 @@ import { Card, CardImg, CardTitle, CardText, Button } from 'reactstrap';
 
 function QuickServiceList(props) {
 
-        const directory = props.quickservices.map(quickservice => {
+        const directory = props.quickServices.map(quickService => {
             return (
-                <div key={quickservice.id} className="col-md-5 m-1">
+                <div key={quickService.id} className="col-md-5 m-1">
                     <Card>
-                        <CardImg height={240} width={240} src={quickservice.image} alt={quickservice.name} />
-                        <CardTitle>{quickservice.name}</CardTitle>
-                        <CardText>{quickservice.description}</CardText>
+                        <CardImg height={240} width={240} src={quickService.image} alt={quickService.name} />
+                        <CardTitle>{quickService.name}</CardTitle>
+                        <CardText>{quickService.description}</CardText>
                         <Button>Let's See the Menu</Button>
                     </Card>
                 </div>
--- Desktop/NucampFolder/4-React-Native/findfoodapp/src/redux/configureStore.js
+++ Desktop/NucampFolder/4-React-Native/findfoodapp/src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, InitialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { foodTypes } from './foodtypes';
+import { quickServices } from './quickservices';
+import { recepies } from './recepies';
+import { restaurants } from './restaurants';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, 
-        InitialState
+        combineReducers({
+            foodTypes: foodTypes,
+            quickServices: quickServices,
+            recepies: recepies,
+            restaurants: restaurants
+        })
     );
     return store;
 }
